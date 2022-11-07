746fc5d3bed00dd857db4cef88916809d178ad79
Rafsanzani Ludhi <rafsanzaniludhi@gmail.com>
2022-11-07 17:39:38 +0000

    removed script tags as it caused error in js file

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-<script>
-    let season = 'summer';
-    let response;
+let season = 'summer';
+let response;
 
-    // Add your code here
+// Add your code here
 
-    // Don't edit the code below here!
+// Don't edit the code below here!
 
-    const section = document.querySelector('section');
+const section = document.querySelector('section');
 
-    let para1 = document.createElement('p');
-    para1.textContent = response;
-    section.appendChild(para1);
-
-</script>
+let para1 = document.createElement('p');
+para1.textContent = response;
+section.appendChild(para1);
