25256dc4d7da296f71264eed0b7eb9953185eaa8
Rafsanzani Ludhi <rafsanzaniludhi@gmail.com>
2022-11-07 17:59:05 +0000

    Added my solution.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+/*global console*/
+
+/*eslint-disabled no-console */
+const section = document.querySelector('section');
+
 let season = 'summer';
 let response;
 
 // Add your code here
-
+switch (season) {
+    case 'summer':
+        response = 'Summer season is hot and humid. Make sure to wear enough sunscreen.'
+        break;
+    case 'winter':
+        response = 'Winter is very cold. Make sure to wear enough layers to keep warm.'
+    default:
+        response = 'I don\t know what season it is'
+        break;
+}
 // Don't edit the code below here!
 
-const section = document.querySelector('section');
 
 let para1 = document.createElement('p');
 para1.textContent = response;
