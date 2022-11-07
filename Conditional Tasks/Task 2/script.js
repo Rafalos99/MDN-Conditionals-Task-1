74f12d29e51a9aea91669ba8f7a6ee5b5950fe36
Rafsanzani Ludhi <rafsanzaniludhi@gmail.com>
2022-11-07 19:32:16 +0000

    Added my solution. Seems to work :D

--- Conditional Tasks/Task 2/script.js
+++ Conditional Tasks/Task 2/script.js
@@ -1,9 +1,26 @@
 let response;
-let score = 75;
-let machineActive = false;
+let score = 71.5;
+let machineActive = true;
 
 // Add your code here
-
+if (machineActive === true) {
+    if (score <0 || score > 100) {
+        response = 'This is not possible, an error has occurred'
+    } else if (score >= 0 && score <= 19) {
+        response = 'That was a terrible score - total fail!'        
+    } else if (score >= 20 && score <= 39) {
+        response = 'You know some things, but it\'s a pretty bad score. Needs improvement.'
+    } else if (score >= 40 && score <= 69) {
+        response = 'You did a passable job, not bad!'
+    } else if (score >= 70 && score <= 89) {
+        response = 'That\'s a great score, you really know your stuff.'
+    } else if (score >= 90 && score <= 100) {
+        response = 'What an amazing score! Did you cheat? Are you for real?'
+    }
+    
+} else if (machineActive === false) {
+    response = 'Switch the machine on';
+}
 // Don't edit the code below here!
 
 const section = document.querySelector('section');
